f4dcea24ead3fc3ce272cef9580238491e9099a9
mhdalfinaja-hash <mhdalfinaja@mhs.unimed.ac.id>
2026-05-14 21:50:59 +0700

    update tampilan baru

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,7 +11,7 @@ import {
   signInAnonymously
 } from 'firebase/auth';
 import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
