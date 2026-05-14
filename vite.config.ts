abc11d02377e5716b53a9a861698afb0c48503ed
mhdalfinaja-hash <mhdalfinaja@mhs.unimed.ac.id>
2026-05-14 21:29:14 +0700

    update tampilan baru

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    optimizeDeps: {
+      include: ['firebase/app', 'firebase/firestore', 'firebase/auth'],
+    },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
